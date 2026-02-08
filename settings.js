d0117930a7e0e511966ed8855c25b427f88e1e9d
ChelseaAdm001 <abikeoluwaseun30@gmail.com>
2026-02-08 02:54:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'chelsea Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "King Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349048958730', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
